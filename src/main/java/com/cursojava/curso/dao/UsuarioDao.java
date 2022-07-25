8d80598ab450ebec68141c266bb0443024e9dcfc
GodAkadioEstudio <ffgalarde@gmail.com>
2022-07-25 00:48:18 -0500

    CRUD de usuarios y login

---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+package com.cursojava.curso.dao;
+
+import java.util.List;
+
+import com.cursojava.curso.models.Usuario;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    boolean verificarCredenciales(Usuario usuario);
+
+}
